0440358aac54768fc8f3c1955a4c58ae6dd72fdc
Sebastian Waloszek <sebastian.waloszek@deviniti.com>
2020-09-09 11:17:24 +0200

    Add custom fonts, colors, styles

--- lib/presentation/theme/app_text_styles.dart
+++ lib/presentation/theme/app_text_styles.dart
@@ -6,4 +6,73 @@ import 'color/app_colors.dart';
 abstract class AppTextStyles {
   static const fontFamily = 'Roboto';
 
+  static TextStyle headline1(BuildContext context, {Color color}) {
+    return TextStyle(
+      color: color ?? AppColors.primaryContent(context),
+      fontSize: 96,
+      letterSpacing: -0.24,
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.normal,
+    );
+  }
+
+  static TextStyle headline2(BuildContext context, {Color color}) {
+    return TextStyle(
+      color: color ?? AppColors.primaryContent(context),
+      fontSize: 96,
+      letterSpacing: -0.24,
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.w300,
+    );
+  }
+
+  static TextStyle headline3(BuildContext context, {Color color}) {
+    return TextStyle(
+      color: color ?? AppColors.primaryContent(context),
+      fontSize: 24,
+      letterSpacing: -0.24,
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle headline4(BuildContext context, {Color color}) {
+    return TextStyle(
+      color: color ?? AppColors.primaryContent(context),
+      fontSize: 20,
+      letterSpacing: -0.24,
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle body1(BuildContext context, {Color color}) {
+    return TextStyle(
+      color: color ?? AppColors.primaryContent(context),
+      fontSize: 24,
+      letterSpacing: -0.24,
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.normal,
+    );
+  }
+
+  static TextStyle body2(BuildContext context, {Color color}) {
+    return TextStyle(
+      color: color ?? AppColors.primaryContent(context),
+      fontSize: 20,
+      letterSpacing: -0.24,
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.normal,
+    );
+  }
+
+  static TextStyle subtitle1(BuildContext context, {Color color}) {
+    return TextStyle(
+      color: color ?? AppColors.primaryContent(context),
+      fontSize: 16,
+      letterSpacing: -0.24,
+      fontFamily: fontFamily,
+      fontWeight: FontWeight.normal,
+    );
+  }
 }
--- lib/presentation/theme/color/theme_colors.dart
+++ lib/presentation/theme/color/theme_colors.dart
@@ -2,6 +2,24 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-abstract class LightThemeColors {}
+abstract class LightThemeColors {
+  static Color get primaryContent => const Color(0xFF222529);
+  static Color get secondaryContent => const Color(0xFFBEBDBD);
+  static Color get primaryAccent => const Color(0xFF);
+  static Color get buttonContent => const Color(0xFFFFFFFF);
+  static Color get background => const Color(0xFFFFFFFF);
+  static Color get secondaryBackground => const Color(0xFFF5F5F5);
+  static Color get active => const Color(0xFF7ECB7A);
+  static Color get shadow => const Color(0xFF000000).withOpacity(0.25);
+}
 
-abstract class DarkThemeColors {}
+abstract class DarkThemeColors {
+  static Color get primaryContent => const Color(0xFFE1E1E1);
+  static Color get secondaryContent => const Color(0xFFA5A5A5);
+  static Color get primaryAccent => const Color(0xFF);
+  static Color get buttonContent => const Color(0xFFFFFFFF);
+  static Color get background => const Color(0xFF181818);
+  static Color get secondaryBackground => const Color(0xFF2B2B2B);
+  static Color get active => const Color(0xFF7ECB7A);
+  static Color get shadow => const Color(0xFF000000).withOpacity(0.25);
+}
