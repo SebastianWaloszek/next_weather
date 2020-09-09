9a8f5a1263e8ac3c0cb4e3fa3d27a3c740496b74
Sebastian Waloszek <sebastian.waloszek@deviniti.com>
2020-09-09 16:27:45 +0200

    Fix theme colors

--- lib/presentation/theme/app_themes.dart
+++ lib/presentation/theme/app_themes.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 import 'color/theme_colors.dart';
 
 final Map<ThemeMode, ThemeData> appThemes = {
-  ThemeMode.light: ThemeData(),
-  ThemeMode.dark: ThemeData(),
+  ThemeMode.light: ThemeData(
+    accentColor: LightThemeColors.primaryAccent,
+    primaryColor: LightThemeColors.primaryAccent,
+    scaffoldBackgroundColor: LightThemeColors.background,
+    cursorColor: LightThemeColors.primaryAccent,
+    canvasColor: Colors.transparent,
+    brightness: Brightness.light,
+  ),
+  ThemeMode.dark: ThemeData(
+    accentColor: DarkThemeColors.primaryAccent,
+    canvasColor: Colors.transparent,
+    scaffoldBackgroundColor: DarkThemeColors.background,
+    cursorColor: DarkThemeColors.primaryAccent,
+    brightness: Brightness.dark,
+  ),
 };
--- lib/presentation/theme/color/theme_colors.dart
+++ lib/presentation/theme/color/theme_colors.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 abstract class LightThemeColors {
   static Color get primaryContent => const Color(0xFF222529);
   static Color get secondaryContent => const Color(0xFFBEBDBD);
-  static Color get primaryAccent => const Color(0xFF);
+  static Color get primaryAccent => const Color(0xFF555555);
   static Color get buttonContent => const Color(0xFFFFFFFF);
   static Color get background => const Color(0xFFFFFFFF);
   static Color get secondaryBackground => const Color(0xFFF5F5F5);
@@ -16,7 +16,7 @@ abstract class LightThemeColors {
 abstract class DarkThemeColors {
   static Color get primaryContent => const Color(0xFFE1E1E1);
   static Color get secondaryContent => const Color(0xFFA5A5A5);
-  static Color get primaryAccent => const Color(0xFF);
+  static Color get primaryAccent => const Color(0xFFAAAAAA);
   static Color get buttonContent => const Color(0xFFFFFFFF);
   static Color get background => const Color(0xFF181818);
   static Color get secondaryBackground => const Color(0xFF2B2B2B);
